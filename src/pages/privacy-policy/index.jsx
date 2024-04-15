4ee30b63bac5c061a0a618f865790d5dd7f3caed
infinitiemitesh <infinitie.mitesh@gmail.com>
2024-04-15 09:49:16 +0530

    privacy policy and terms and condtionds fixed

--- src/pages/privacy-policy/index.jsx
+++ src/pages/privacy-policy/index.jsx
@@ -1,40 +1,58 @@
-"use client";
-import React, { useEffect } from 'react'
-import BreadCrumb from '@/component/BreadCrumb/BreadCrumb';
-import Products from '@/views/Products';
-import { Box } from '@mui/joy';
+import React, { useEffect, useState } from 'react';
+import { Box, Typography, List, ListItem } from '@mui/material';
 import { HeadTitle } from "@/component/HeadTitle";
 import { useSelector } from 'react-redux';
 import { getSettings } from '@/store/reducers/settingsSlice';
+import dynamic from 'next/dynamic';
+import parse, { domToReact } from 'html-react-parser'; // Import the parse and domToReact functions from html-react-parser
 
 const index = () => {
-
+    const BreadCrumb = dynamic(() => import("@/component/BreadCrumb/BreadCrumb"), {
+        ssr: false,
+    });
 
     const settings = useSelector((state) => state.settings.value);
-    const [setting, setSettings] = React.useState(false);
+    const [setting, setSettings] = useState(null);
+
     useEffect(() => {
-
-        if (settings && settings?.web_settings.length != 0) setSettings(settings.privacy_policy[0])
-
+        if (settings && settings.web_settings.length !== 0) {
+            setSettings(settings.privacy_policy[0]);
+        }
     }, [settings]);
 
+    // Custom replace function to replace certain HTML elements with Material-UI components
+    const replaceElement = (domNode, index) => {
+        if (domNode.type === 'tag') {
+            switch (domNode.name) {
+                case 'h1':
+                    return <Typography  sx={{ mt: 2, mb: 2 }}>{domToReact(domNode.children)}</Typography>;
+                case 'h2':
+                    return <Typography  sx={{ mt: 2, mb: 2 }}>{domToReact(domNode.children)}</Typography>;
+                case 'p':
+                    return <Typography  sx={{ mt: 1, mb: 1 }}>{domToReact(domNode.children)}</Typography>;
+                case 'ul':
+                    return <List sx={{ mt: 1, mb: 1 }}>{domToReact(domNode.children)}</List>;
+                case 'li':
+                    return <ListItem sx={{ pl: 2 }}>{domToReact(domNode.children)}</ListItem>;
+                // Add more cases for other HTML elements you want to replace
+                default:
+                    return domToReact(domNode.children);
+            }
+        }
+    };
 
     return (
         <Box>
             <HeadTitle title={"privacy-policy"} />
-
             <Box>
                 <BreadCrumb page={[{ name: "privacy-policy", link: "#" }]} />
             </Box>
-
-            <box>
-
-                <pre>
-                    <div dangerouslySetInnerHTML={{ __html: setting ? setting : "" }} />
-                </pre>
-            </box>
+            <Box>
+                {/* Parse the HTML content and replace certain elements with Material-UI components */}
+                {setting && parse(setting, { replace: replaceElement })}
+            </Box>
         </Box>
-    )
-}
+    );
+};
 
-export default index
+export default index;
--- src/pages/terms-conditions/index.jsx
+++ src/pages/terms-conditions/index.jsx
@@ -1,41 +1,85 @@
 "use client";
-import React, { useEffect } from 'react'
-import BreadCrumb from '@/component/BreadCrumb/BreadCrumb';
-import Products from '@/views/Products';
-import { Box } from '@mui/joy';
+import React, { useEffect } from "react";
+import Products from "@/views/Products";
+import { Box, List, ListItem, Typography } from "@mui/joy";
 import { HeadTitle } from "@/component/HeadTitle";
-import { useSelector } from 'react-redux';
-import { getSettings } from '@/store/reducers/settingsSlice';
+import { useSelector } from "react-redux";
+import { getSettings } from "@/store/reducers/settingsSlice";
+import dynamic from "next/dynamic";
+import parse, { domToReact } from "html-react-parser"; // Import the parse and domToReact functions from html-react-parser
 
 const index = () => {
+  const BreadCrumb = dynamic(
+    () => import("@/component/BreadCrumb/BreadCrumb"),
+    {
+      ssr: false,
+    }
+  );
+  const settings = useSelector((state) => state.settings.value);
+  const [setting, setSettings] = React.useState(false);
+  useEffect(() => {
+    if (settings && settings?.web_settings.length != 0)
+      setSettings(settings.terms_conditions[0]);
+  }, [settings]);
 
+  // Custom replace function to replace certain HTML elements with Material-UI components
+  const replaceElement = (domNode, index) => {
+    if (domNode.type === "tag") {
+      switch (domNode.name) {
+        case "h1":
+          return (
+            <Typography sx={{ mt: 2, mb: 2 }}>
+              {domToReact(domNode.children)}
+            </Typography>
+          );
+        case "h2":
+          return (
+            <Typography sx={{ mt: 2, mb: 2 }}>
+              {domToReact(domNode.children)}
+            </Typography>
+          );
+        case "h3":
+          return (
+            <Typography sx={{ mt: 2, mb: 2 }}>
+              {domToReact(domNode.children)}
+            </Typography>
+          );
 
-    const settings = useSelector((state) => state.settings.value);
-    const [setting, setSettings] = React.useState(false);
-    useEffect(() => {
+        case "p":
+          return (
+            <Typography sx={{ mt: 1, mb: 1 }}>
+              {domToReact(domNode.children)}
+            </Typography>
+          );
+        case "ul":
+          return (
+            <List sx={{ mt: 1, mb: 1 }}>{domToReact(domNode.children)}</List>
+          );
+        case "li":
+          return (
+            <ListItem sx={{ pl: 2 }}>{domToReact(domNode.children)}</ListItem>
+          );
+        // Add more cases for other HTML elements you want to replace
+        default:
+          return domToReact(domNode.children);
+      }
+    }
+  };
 
-        if (settings && settings?.web_settings.length != 0) setSettings(settings.terms_conditions[0])
+  return (
+    <Box>
+      <HeadTitle title={"terms-conditions"} />
 
-    }, [settings]);
+      <Box>
+        <BreadCrumb page={[{ name: "terms-conditions", link: "#" }]} />
+      </Box>
 
+      <Box>
+        {/* Parse the HTML content and replace certain elements with Material-UI components */}
+        {setting && parse(setting, { replace: replaceElement })}
+      </Box>
+    </Box>
+  );
+};
 
-    return (
-        <Box>
-            <HeadTitle title={"terms-conditions"} />
-
-            <Box>
-                <BreadCrumb page={[{ name: "terms-conditions", link: "#" }]} />
-            </Box>
-
-
-            <Box>
-                {/* <pre> */}
-                <div dangerouslySetInnerHTML={{ __html: setting ? setting : "" }} />
-                {/* </pre> */}
-
-            </Box>
-        </Box>
-    )
-}
-
-export default index
+export default index;
